7d0cca96571c3042e8b30142ca5f211053b550bc
tanimj <111261903+tanimj@users.noreply.github.com>
2024-08-28 21:17:17 -0600

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,6 @@
+
+
+      document.getElementById('menu-icon').addEventListener('click', function() {
+        var menu = document.getElementById('menu');
+        menu.classList.toggle('active');
+    });
